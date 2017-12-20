ef0c3113458fd204e9b2eb6316e9e4c302bb3c49
Jeffery Huang <jeffery@mymac.local>
2017-12-20 00:12:35 -0800

    Initial Commit

---
+++ messenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  messenger
+//
+//  Created by Qichen Huang on 2017-12-20.
+//  Copyright © 2017 Qichen Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
